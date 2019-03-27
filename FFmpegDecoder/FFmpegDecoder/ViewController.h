ee919316c21038e19c338eaaf35f8f53510fd2d9
luowailin <blazer402@126.com>
2019-03-27 16:35:32 +0800

    解码器

---
+++ FFmpegDecoder/FFmpegDecoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFmpegDecoder
+//
+//  Created by luowailin on 2019/3/27.
+//  Copyright © 2019 luowailin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FFmpegDecoder/FFmpegDecoder/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  FFmpegDecoder
+//
+//  Created by luowailin on 2019/3/27.
+//  Copyright © 2019 luowailin. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <libavcodec/avcodec.h>
+#import <libavformat/avformat.h>
+#import <libavutil/imgutils.h>
+#import <libswscale/swscale.h>
+
+
+@interface ViewController ()
+
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
